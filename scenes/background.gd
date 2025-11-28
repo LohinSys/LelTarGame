d066d3a9d7d3ee09a3339195feb8096a6198b827
sz5ylv1a <sylvia.szalai.uwu@pm.me>
2025-11-28 11:37:17 +0100

    actual scrolling bg let's gooooo

--- scenes/background.gd
+++ scenes/background.gd
@@ -1,12 +1,16 @@
 extends Node3D
 
 func _physics_process(_delta: float) -> void:
-	$StaticBody3D.rotate_x(0.02)
-	$StaticBody3D.rotate_y(0.02)
-	$StaticBody3D.rotate_z(0.02)
-	#$Layer1.move_local_y(10)
-	#$Layer2.move_local_y(30)
-	#if $Layer1.position == Vector2(0,1200):
-		#$Layer1.position = Vector2(0,0)
-	#if $Layer2.position == Vector2(0,2400):
-		#$Layer2.position = Vector2(0,-1200)
+	#$TestCube3D.rotate_x(0.02)
+	#$TestCube3D.rotate_y(0.03)
+	#$TestCube3D.rotate_z(0.05)
+
+	$"Ground3D-1".move_and_collide(Vector3(0,0,0.05))
+	$"Ground3D-2".move_and_collide(Vector3(0,0,0.05))
+	$"Ground3D-3".move_and_collide(Vector3(0,0,0.05))
+	if $"Ground3D-1".position >= Vector3(0,-1,20):
+		$"Ground3D-1".position = Vector3(0,-1,-39)
+	if $"Ground3D-2".position >= Vector3(0,-1,20):
+		$"Ground3D-2".position = Vector3(0,-1,-39)
+	if $"Ground3D-3".position >= Vector3(0,-1,20):
+		$"Ground3D-3".position = Vector3(0,-1,-39)
