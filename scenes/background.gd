5311b5dcba423f3fea1ed2ad11c615143c5f29b3
sz5ylv1a <sylvia.szalai.uwu@pm.me>
2026-02-16 15:05:23 +0100

    bump version to 0.2.14 and improve bg, as well as switching to forward+ renderer

--- scenes/background.gd
+++ scenes/background.gd
@@ -1,12 +1,26 @@
 extends Node3D
 
-func _physics_process(_delta: float) -> void:
-	$"Ground3D-1".move_and_collide(Vector3(0,0,0.05))
-	$"Ground3D-2".move_and_collide(Vector3(0,0,0.05))
-	$"Ground3D-3".move_and_collide(Vector3(0,0,0.05))
-	if $"Ground3D-1".position >= Vector3(0,-1,20):
-		$"Ground3D-1".position = Vector3(0,-1,-39)
-	if $"Ground3D-2".position >= Vector3(0,-1,20):
-		$"Ground3D-2".position = Vector3(0,-1,-39)
-	if $"Ground3D-3".position >= Vector3(0,-1,20):
-		$"Ground3D-3".position = Vector3(0,-1,-39)
+var camera_rotation: float = 0.001
+
+func _physics_process(_delta) -> void:
+	# keep moving the background
+	$"Ground3D-1".move_and_collide(Vector3(0,0,0.025))
+	$"Ground3D-2".move_and_collide(Vector3(0,0,0.025))
+	$"Ground3D-3".move_and_collide(Vector3(0,0,0.025))
+	# reset position so it loops infinitely
+	if $"Ground3D-1".position.z >= 20:
+		$"Ground3D-1".position.z = -39
+	if $"Ground3D-2".position.z >= 20:
+		$"Ground3D-2".position.z = -39
+	if $"Ground3D-3".position.z >= 20:
+		$"Ground3D-3".position.z = -39
+
+	# bob around the camera
+	$Camera3D.rotate_z(camera_rotation/2)
+	$Camera3D.position.x -= float((camera_rotation*10)/2)
+	if $Camera3D.rotation.z >= (19.0 / 100.0):
+		if camera_rotation >= -0.001:
+			camera_rotation -= 0.00001
+	if $Camera3D.rotation.z <= (-19.0 / 100.0):
+		if camera_rotation <= 0.001:
+			camera_rotation += 0.00001
